2ab45352923e2cc570b214c2d1cf0c53df0174e0
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-12-10 00:16:55 +0600

    added hot jobs card

--- career-code-client/src/pages/Home/HotJobs.jsx
+++ career-code-client/src/pages/Home/HotJobs.jsx
@@ -1,11 +1,12 @@
 import { use } from "react";
+import JobCard from "../Shared/JobCard";
 
 const HotJobs = ({ jobsPromise }) => {
     // const jobs = use(jobsPromise);
     return (
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {
-                jobsPromise.length
+                jobsPromise.map((jobs) => <JobCard key={jobs._id} jobs={jobs}></JobCard>)
             }
         </div>
     );
