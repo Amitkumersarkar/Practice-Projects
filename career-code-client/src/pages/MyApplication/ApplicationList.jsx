2b2c080da2ba1d2403f55d2640193063681242d9
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-12-13 22:36:26 +0600

    fixes error

--- career-code-client/src/pages/MyApplication/ApplicationList.jsx
+++ career-code-client/src/pages/MyApplication/ApplicationList.jsx
@@ -1,9 +1,24 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../contextApi/AuthContext/AuthContext";
+import { myApplicationPromise } from "../../applicationApis/applicationApis";
 
 const ApplicationList = () => {
+    const { user } = useContext(AuthContext);
+    const [applications, setApplications] = useState([]);
+
+    useEffect(() => {
+        if (!user?.email) return;
+
+        myApplicationPromise(user.email)
+            .then(data => setApplications(data))
+            .catch(err => console.error(err));
+    }, [user?.email]);
 
     return (
         <div>
-
+            <h2 className="text-3xl">
+                Jobs Applied so far: {applications.length}
+            </h2>
         </div>
     );
 };
--- career-code-client/src/pages/MyApplication/MyApplication.jsx
+++ career-code-client/src/pages/MyApplication/MyApplication.jsx
@@ -1,12 +1,19 @@
+import { Suspense } from "react";
 import ApplicationList from "./ApplicationList";
 import ApplicationStats from "./ApplicationStats";
+import UserAuth from "../../hooks/UserAuth";
+import { myApplicationPromise } from "../../applicationApis/applicationApis";
 
 const MyApplication = () => {
+    const { user } = UserAuth();
 
     return (
         <div>
             <ApplicationStats></ApplicationStats>
-            <ApplicationList></ApplicationList>
+            <Suspense fallback={'loading your applications'}>
+                <ApplicationList myApplicationPromise={myApplicationPromise}>
+                </ApplicationList>
+            </Suspense>
         </div>
     );
 };
