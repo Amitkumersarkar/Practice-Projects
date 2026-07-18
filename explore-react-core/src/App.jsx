0ada629148ecaf12ba8088121f059cf750c4f83a
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-07-18 23:32:50 +0600

    updated file

--- explore-react-core/src/App.jsx
+++ explore-react-core/src/App.jsx
@@ -2,16 +2,21 @@ function App() {
 
   return (
     <>
-      {/* <h1>Hello Developer</h1> */}
-      <Person></Person>
+      <h1>Hello Developer</h1>
+      <Person ></Person>
+      <Device name="Laptop"></Device>
     </>
   )
+}
+function Device(props) {
+  console.log(props);
+  return <h2>Laptop</h2>
 }
 function Person() {
   const age = 22;
   const name = 'jon';
   return (
-    <p>my name is {name}. iam {age} iam a Developer</p>
+    <p style={{ margin: '10px 20px 30px 40px', color: 'skyblue', fontSize: '30px' }}>my name is {name}. iam {age} iam a Developer</p>
   )
 }
 
