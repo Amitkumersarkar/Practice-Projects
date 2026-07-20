d4e87995a4c7245de2a7b5f1c5f66239fcfd9d1a
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-07-20 23:20:38 +0600

    updated file

--- explore-react-core/src/App.jsx
+++ explore-react-core/src/App.jsx
@@ -1,29 +1,11 @@
-import Todo from "./todo";
 
-function App() {
+const App = () => {
 
   return (
-    <>
-      <h1>Hello Developer</h1>
-      <Person ></Person>
-      <Device name="Laptop" price="50000"></Device>
-      <Device name="Mobile" price="25000"></Device>
-      <Device name="Watch" price="2500"></Device>
-      <Todo></Todo>
-    </>
-  )
-}
-function Device(props) {
-  console.log(props);
-  return <h2>Device : {props.name} <br /> <p style={{ marginTop: "10px" }}>Price {props.price}</p> </h2>
+    <div>
 
-}
-function Person() {
-  const age = 22;
-  const name = 'jon';
-  return (
-    <p style={{ margin: '10px 20px 30px 40px', color: 'skyblue', fontSize: '30px' }}>my name is {name}. iam {age} iam a Developer</p>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
