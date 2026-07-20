0a440524dfed9f734051480223e30674dd4b2e0f
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-07-20 23:58:48 +0600

    arrow functions

--- explore-react-core/src/App.jsx
+++ explore-react-core/src/App.jsx
@@ -1,9 +1,23 @@
 
 const App = () => {
-
+  function handleClick() {
+    alert('Hello');
+  }
+  const handleClick2 = () => {
+    alert('Click two');
+  }
+  const handleAddNum = (num) => {
+    const newNum = num + 5;
+    alert(newNum);
+  }
   return (
     <div>
-
+      <h1>Hello developer</h1>
+      <button onClick={handleClick} style={{ color: " skyblue", fontSize: "30px" }}>Click Here</button>
+      <br />
+      <button onClick={handleClick2} style={{ gap: "200px", marginTop: "10px", color: " skyblue", fontSize: "20px" }}>Click Here</button>
+      <br />
+      <button onClick={() => handleAddNum(10)}>Add Five</button>
     </div>
   );
 };
