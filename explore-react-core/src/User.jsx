e2a0f9dceb9c67d4f11a502e8c934c6efab13521
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-07-24 23:57:36 +0600

    loaded data

--- explore-react-core/src/User.jsx
+++ explore-react-core/src/User.jsx
@@ -1,9 +1,11 @@
 
-const User = () => {
-
+const User = ({ user }) => {
+    const { name, email, id } = user;
     return (
         <div>
-
+            <h2>ID : {id}</h2>
+            <h3>Name : {name} </h3>
+            <p>Email : {email}</p>
         </div>
     );
 };
--- explore-react-core/src/Users.jsx
+++ explore-react-core/src/Users.jsx
@@ -1,11 +1,15 @@
 import { use } from "react";
+import User from "./User";
 
 const Users = ({ fetchUsers }) => {
     const users = use(fetchUsers);
     console.log(users);
     return (
-        <div>
+        <div className="card">
             <h3>Users : {users.length}</h3>
+            {
+                users.map(user => <User user={user}></User>)
+            }
         </div>
     );
 };
