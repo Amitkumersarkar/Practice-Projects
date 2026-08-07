2556c5b6e0f46777bfec861f7c28ceb784274c7f
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-08-07 19:22:25 +0600

    updated file

--- rest_countries/src/App.jsx
+++ rest_countries/src/App.jsx
@@ -1,7 +1,6 @@
 import { Suspense } from "react";
 import Countries from "./components/Countries/Countries";
 
-// loading data
 const countriesPromise = fetch('https://openapi.programming-hero.com/api/all')
   .then(res => res.json())
 
